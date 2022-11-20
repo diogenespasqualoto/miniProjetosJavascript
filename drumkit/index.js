cc7e42f26534a25c12829f66172301b15f5e4033
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2022-11-20 07:46:40 -0300

    fix bug on sounds

--- drumkit/index.js
+++ drumkit/index.js
@@ -1,5 +1,4 @@
-'use strict'
-
+'use strict';
 
 const sons = {
     'A': 'boom.wav',
@@ -23,5 +22,35 @@ const criarDiv = (texto) => {
 
 const exibir = (sons) => Object.keys(sons).forEach(criarDiv);
 
+const tocarSom = (letra) => {
+    const audio = new Audio(`./sounds/${sons[letra]}`);
+    audio.play();
+}
+
+const adicionarEfeito = (letra) => document.getElementById(letra)
+                                           .classList.toggle('active');
+
+const removerEfeito = (letra) => {
+    const div = document.getElementById(letra);
+    const removeActive = () => div.classList.remove('active');
+    div.addEventListener('transitionend',removeActive);
+};
+
+const ativarDiv = (evento) => {
+
+    const letra = evento.type == 'click' ? evento.target.id : evento.key.toUpperCase();
+    
+    const letraPermitida = sons.hasOwnProperty(letra);
+    if (letraPermitida){
+        adicionarEfeito(letra);
+        tocarSom(letra);
+        removerEfeito(letra);
+    }
+}
+
 
 exibir(sons);
+document.getElementById('container')
+        .addEventListener('click', ativarDiv);
+
+window.addEventListener('keyup',ativarDiv);

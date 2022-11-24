ddbdaf3fb406bbe6c940aeef5961e89918d29082
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2022-11-24 07:51:56 -0300

    Limpar display apos selecionar operador está funcionando

---
+++ calculadora/calculadora.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const display = document.getElementById('display');
+const numeros = document.querySelectorAll('[id*=tecla]');
+const operadores = document.querySelectorAll('[id*=operador]');
+
+let novoNumero = true;
+
+const atualizarDisplay = (texto) => {
+    if (novoNumero) {
+        display.textContent = texto;
+        novoNumero = false; return
+    } else {
+        display.textContent += texto;
+    }
+    display.textContent += texto;
+}
+
+const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
+numeros.forEach(numero => numero.addEventListener('click', inserirNumero));
+
+const selecionarOperador = () => { novoNumero = true; };
+operadores.forEach(operador => operador.addEventListener('click', selecionarOperador));

ce7ad9dfe1065d8c866a383c734528c39d43788a
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2022-11-24 08:12:35 -0300

    Dinamica da soma está completa

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -5,19 +5,40 @@ const numeros = document.querySelectorAll('[id*=tecla]');
 const operadores = document.querySelectorAll('[id*=operador]');
 
 let novoNumero = true;
+let operador;
+let numeroAnterior;
+
+const operacaoPendente = () => operador !== undefined;
+
+const calcular = () => {
+    if (operacaoPendente()) {
+        const numeroAtual = parseFloat(display.textContent);
+        novoNumero = true;
+        if (operador == '+') {
+            atualizarDisplay(numeroAnterior + numeroAtual);
+        }
+    }
+}
 
 const atualizarDisplay = (texto) => {
     if (novoNumero) {
         display.textContent = texto;
-        novoNumero = false; return
+        novoNumero = false;
     } else {
         display.textContent += texto;
     }
-    display.textContent += texto;
 }
 
 const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
 numeros.forEach(numero => numero.addEventListener('click', inserirNumero));
 
-const selecionarOperador = () => { novoNumero = true; };
+const selecionarOperador = (evento) => {
+    if (!novoNumero) {
+        calcular();
+        novoNumero = true;
+        operador = evento.target.textContent;
+        numeroAnterior = parseFloat(display.textContent);
+        console.log(operador)
+    }
+};
 operadores.forEach(operador => operador.addEventListener('click', selecionarOperador));

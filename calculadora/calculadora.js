ced11453038efcdf5498ef20e529c1c0bcf91e9e
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2022-11-25 07:00:29 -0300

    Adicionando regra para filtrar teclas permitidas

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -12,7 +12,7 @@ const operacaoPendente = () => operador !== undefined;
 
 const calcular = () => {
     if (operacaoPendente()) {
-        const numeroAtual = parseFloat(display.textContent);
+        const numeroAtual = parseFloat(display.textContent.replace(',', '.'));
         novoNumero = true;
         const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`)
         atualizarDisplay(resultado);
@@ -36,8 +36,61 @@ const selecionarOperador = (evento) => {
         calcular();
         novoNumero = true;
         operador = evento.target.textContent;
-        numeroAnterior = parseFloat(display.textContent);
-        console.log(operador)
+        numeroAnterior = parseFloat(display.textContent.replace(',', '.'));
     }
 };
 operadores.forEach(operador => operador.addEventListener('click', selecionarOperador));
+
+const ativarIgual = () => {
+    calcular();
+    operador = undefined;
+}
+
+document.getElementById('igual').addEventListener('click', ativarIgual);
+
+const limparDisplay = () => display.textContent = '';
+document.getElementById('limparDisplay').addEventListener('click', limparDisplay);
+
+const limparCalculo = () => {
+    limparDisplay();
+    operador = undefined;
+    numeroAnterior = undefined;
+}
+
+document.getElementById('limparCalculo').addEventListener('click', limparCalculo);
+
+const removerUltimoNumero = () => display.textContent = display.textContent.slice(0, -1)
+document.getElementById('backspace').addEventListener('click', removerUltimoNumero);
+
+const inverterSinal = () => {
+    novoNumero = true;
+    atualizarDisplay(display.textContent * -1);
+}
+document.getElementById('inverter').addEventListener('click', inverterSinal);
+
+const existeDecimal = () => display.textContent.indexOf(',') !== -1;
+const existeValor = () => display.textContent.length > 0;
+const inserirDecimal = () => {
+    if (!existeDecimal()) {
+        if (existeValor()) {
+            atualizarDisplay(',');
+        } else {
+            atualizarDisplay('0,');
+        }
+    }
+}
+document.getElementById('decimal').addEventListener('click', inserirDecimal);
+
+const mapaTeclado = {
+    0: 'tecla0',
+    1: 'tecla1',
+    2: 'tecla2',
+}
+
+const mapearTeclado = (evento) => {
+    const tecla = evento.key;
+
+    const teclaPermitida = () => Object.keys(mapaTeclado).indexOf(tecla) !== -1;
+    if (teclaPermitida()) document.getElementById(mapaTeclado[tecla]).click();
+};
+document.addEventListener('keydown', mapearTeclado);

741ab5550d89b7b4fda63c676fc6b866731ff6ae
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2022-11-27 11:57:11 -0300

    Salvando dados no localStorage

--- todoList/App.js
+++ todoList/App.js
@@ -1,20 +1,18 @@
 'use strict';
 
-let banco = [
-    { 'tarefa': 'estudar JS', 'status': '' },
-    { 'tarefa': 'Netflix', 'status': 'checked' },
-    { 'tarefa': 'teste1', 'status': '' }
+let banco = [];
 
-];
+const getBanco = () => JSON.parse(localStorage.getItem ('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem ('todoList', JSON.stringify(banco));
 
-const criarNovaLabel = (tarefa, status, indice) => {
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = `
-    <input type="checkbox" ${status} data-indice=${indice} >
-    <div class="">${tarefa}</div>
-    <input type="button" value="X" data-indice=${indice} >
-    `
+        <input type="checkbox" ${status} data-indice=${indice}>
+        <div>${tarefa}</div>
+        <input type="button" value="X" data-indice=${indice}>
+    `;
     document.getElementById('todoList').appendChild(item);
 }
 
@@ -27,36 +25,45 @@ const limparTarefas = () => {
 
 const atualizarTela = () => {
     limparTarefas();
-    banco.forEach((item, indice) => criarNovaLabel(item.tarefa, item.status, indice));
+    const banco = getBanco(); 
+    banco.forEach ( (item, indice) => criarItem (item.tarefa, item.status, indice));
 }
 
 const inserirItem = (evento) => {
     const tecla = evento.key;
     const texto = evento.target.value;
-    if (tecla === 'Enter') {
-        banco.push({ 'tarefa': texto, 'status': '' });
+    if (tecla === 'Enter'){
+        const banco = getBanco();
+        banco.push ({'tarefa': texto, 'status': ''});
+        setBanco(banco);
         atualizarTela();
         evento.target.value = '';
     }
 }
 
 const removerItem = (indice) => {
-    banco.splice(indice, 1);
+    const banco = getBanco();
+    banco.splice (indice, 1);
+    setBanco(banco);
     atualizarTela();
 }
+
 const atualizarItem = (indice) => {
+    const banco = getBanco();
     banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+    setBanco(banco);
     atualizarTela();
 }
+
 const clickItem = (evento) => {
     const elemento = evento.target;
-    console.log(elemento.type);
+    console.log (elemento.type);
     if (elemento.type === 'button') {
         const indice = elemento.dataset.indice;
-        removerItem(indice);
-    } else if (elemento.type === 'checkbox') {
+        removerItem (indice);
+    }else if (elemento.type === 'checkbox') {
         const indice = elemento.dataset.indice;
-        atualizarItem(indice);
+        atualizarItem (indice);
     }
 }
 
@@ -64,5 +71,3 @@ document.getElementById('newItem').addEventListener('keypress', inserirItem);
 document.getElementById('todoList').addEventListener('click', clickItem);
 
 atualizarTela();
-
-
